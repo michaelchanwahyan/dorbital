723bf5827c449d532cbe307283f17fa7adfc067c
michael <michaelchan_wahyan@yahoo.com.hk>
2020-01-21 14:22:44 +0800

    [bugfix] fix x11-unix path volume problem

--- rundocker.sh
+++ rundocker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 clear
+docker rm -f sleepy_jirachi
 export TZ=Asia/Hong_Kong
 mem=$(free -g | head -2 | tail -1 | awk -F " " '{print $2}') # if you run to error, set mem to your system memory (in GB unit)
 target_mem=$(echo "$mem * 0.9" | bc) # if you run to error, install bc
@@ -8,6 +9,7 @@ docker run -dt \
            --name=sleepy_jirachi \
            --memory="$target_mem"g \
            -e DISPLAY=$DISPLAY \
-           -v /tmp/.X11-unix:/tmp/.XIM-unix \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           -v /data:/data \
            dorbital:latest \
            systemd
